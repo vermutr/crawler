e0c7db7f3163190a4b0420ad01a2241ee9abd3fc
vermutr <vertinsky.vermut@gmail.com>
2021-02-09 20:38:49 +0200

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,13 +15,10 @@ public class Main {
                 "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     public static void main(String[] args) throws Exception {
         List<String> refList = new ArrayList<>();
-
         String regex1 = "https://zakupki.gov.ru/.*";
         String regex2 = ".*common-info.html\\?regNumber=.*";
         String regex3 = "https://zakupki.gov.ru/epz/order/notice//*";
-        String regex4 = ".*@.*";
-        String regex5 = ".*.ru";
-        for (int i = 1; i <= 1; i++) {
+        for (int i = 1; i <= 100; i++) {
             String myStr="https://zakupki.gov.ru/epz/order/extendedsearch/results.html?morphology=on&pageNumber=" + i + "&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&ca=on&pc=on&pa=on&priceContractAdvantages44IdNameHidden=%7B%7D&priceContractAdvantages94IdNameHidden=%7B%7D&currencyIdGeneral=-1&customerPlace=5277327&delKladrIds=5277327&selectedSubjectsIdNameHidden=%7B%7D&OrderPlacementSmallBusinessSubject=on&OrderPlacementRnpData=on&OrderPlacementExecutionRequirement=on&orderPlacement94_0=0&orderPlacement94_1=0&orderPlacement94_2=0&contractPriceCurrencyId=-1&budgetLevelIdNameHidden=%7B%7D&nonBudgetTypesIdNameHidden=%7B%7D";
             Document doc = Jsoup.connect(myStr).get();
             Elements element = doc.select("a");
